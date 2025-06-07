866955b2e453bdb5ce4a9351d8a7f154a11e4399
Utsavsingh2826 <utsavsingh2826@gmail.com>
2025-06-07 13:55:04 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import mongoose from 'mongoose';
 import projectModel from './models/project.model.js';
 import { generateResult } from './services/ai.service.js';
 import cors from "cors"
+import connect from './db/db.js';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
